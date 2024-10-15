0dd156589adb64101a53042ca3eb15ddb630e58a
Patrick Souza <159404108+PatrickSouza2245@users.noreply.github.com>
2024-10-14 23:27:47 -0300

    Set host option on listen

--- server.js
+++ server.js
@@ -64,10 +64,7 @@ server.delete('/videos/:id', async (req, res) => {
 
 
 server.listen({
+    host: '0.0.0.0',
     port: process.env.PORT ?? 8080,
-}).then(() => {
-    console.log('Servidor rodando!')
-}).catch((err) => {
-    console.log('Ocorreu um erro ao se conectar ' + err)
 })
 
