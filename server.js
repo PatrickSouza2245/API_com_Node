71e1b0660943ec6822ff6c79dfd07c04e0b20353
Patrick Souza <159404108+PatrickSouza2245@users.noreply.github.com>
2024-10-14 23:07:36 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,73 @@
+// import { createServer } from 'node:http'
+
+// const server = createServer((req, res) => {
+//     res.write('ola')
+
+//     return res.end()
+// })
+
+// server.listen(8080)
+
+import { fastify } from 'fastify'
+// import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js'
+
+
+const server = fastify()
+// const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+server.post('/videos', async (req, res) => {
+    const {title, description, duration} = req.body
+    
+
+    await database.create({
+        title,
+        description,
+        duration
+    })
+
+    console.log(database.list())
+
+    return res.status(201).send()
+})
+
+server.get('/videos', async (req) => {
+    const search = req.query.search
+
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (req, res) => {
+    const videoId = req.params.id 
+    const {title, description, duration} = req.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return res.status(204).send()
+})
+
+server.delete('/videos/:id', async (req, res) => {
+    const videoId = req.params.id
+
+    await database.delete(videoId)
+
+    return res.status(204).send()
+})
+
+
+
+server.listen({
+    port: process.env.PORT ?? 8080,
+}).then(() => {
+    console.log('Servidor rodando!')
+}).catch((err) => {
+    console.log('Ocorreu um erro ao se conectar ' + err)
+})
+
